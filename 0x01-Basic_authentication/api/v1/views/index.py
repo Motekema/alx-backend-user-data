9f7cfe034784a8552b735a92eebba314a76231b0
T.K Mahlanya <91951223+Motekema@users.noreply.github.com>
2024-05-21 11:20:23 +0200

    Update index.py

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -4,6 +4,7 @@
 from flask import jsonify, abort
 from api.v1.views import app_views
 
+
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
     """ 
@@ -12,6 +13,7 @@ def status() -> str:
       - the status of API
     """
     return jsonify({"status": "OK"})
+    
 
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
@@ -25,6 +27,7 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
+
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized() -> str:
     """ 
@@ -33,6 +36,7 @@ def unauthorized() -> str:
       - raises 401 error by using abort
     """
     abort(401)
+
 
 @app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
 def forbidden() -> str:

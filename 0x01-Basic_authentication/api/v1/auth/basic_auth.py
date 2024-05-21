7d3dd9b24ceb5572025061e3201aaab47d073a32
T.K Mahlanya <91951223+Motekema@users.noreply.github.com>
2024-05-21 12:28:38 +0200

    Update basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,7 +8,7 @@ class BasicAuth(Auth):
     """ BasicAuth class that inherits from Auth """
 
     def extract_base64_authorization_header(
-        self, authorization_header: str) -> str:
+            self, authorization_header: str) -> str:
         """ Extracts the Base64 part of the Authorization """
         if authorization_header is None:
             return None

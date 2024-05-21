5a4c98ebed1dd3bec140759388fa717225b9bae9
T.K Mahlanya <91951223+Motekema@users.noreply.github.com>
2024-05-21 12:38:54 +0200

    Update basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,11 +2,38 @@
 """ Basic Authentication module
 """
 from api.v1.auth.auth import Auth
+import base64
 from models.user import User
 from typing import TypeVar
 
 class BasicAuth(Auth):
     """ BasicAuth class that inherits from Auth """
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """ Decode the Base64 authorization header """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            decoded_str = decoded_bytes.decode('utf-8')
+            return decoded_str
+        except base64.binascii.Error:
+            return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """ Extract user credentials Base64 decoded authorization header """
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+        email, password = decoded_base64_authorization_header.split(':', 1)
+        return email, password
 
     def user_object_from_credentials(
             self, user_email: str, user_pwd: str) -> TypeVar('User'):

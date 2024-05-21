1826cc886b3da25c0c0931310b5889a146ac2503
T.K Mahlanya <91951223+Motekema@users.noreply.github.com>
2024-05-21 11:48:38 +0200

    Update auth.py

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,12 +10,29 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Method to check if authentication is required """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
 
+        # Ensure trailing slash consistency
+        if path[-1] != '/':
+            path += '/'
+
+        for ex_path in excluded_paths:
+            if ex_path[-1] != '/':
+                ex_path += '/'
+            if path == ex_path:
+                return False
+
+        return True
+
+    
     def authorization_header(self, request=None) -> str:
         """ Method to get the authorization header """
         return None
 
+    
     def current_user(self, request=None) -> TypeVar('User'):
         """ Method to get the current user """
         return None

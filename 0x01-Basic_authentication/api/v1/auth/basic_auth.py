82a3156e3c6eb5d64aea12bbb2cf94e3f05807bc
T.K Mahlanya <91951223+Motekema@users.noreply.github.com>
2024-05-21 12:30:41 +0200

    Update basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,19 +2,24 @@
 """ Basic Authentication module
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
     """ BasicAuth class that inherits from Auth """
 
-    def extract_base64_authorization_header(
-            self, authorization_header: str) -> str:
-        """ Extracts the Base64 part of the Authorization """
-        if authorization_header is None:
+    def decode_base64_authorization_header(
+        self, base64_authorization_header: str) -> str:
+        """ Decode the Base64 authorization header """
+        if base64_authorization_header is None:
             return None
-        if not isinstance(authorization_header, str):
+        if not isinstance(base64_authorization_header, str):
             return None
-        if not authorization_header.startswith("Basic "):
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            decoded_str = decoded_bytes.decode('utf-8')
+            return decoded_str
+        except:
             return None
-        # Remove "Basic " prefix
+
         return authorization_header[6:]

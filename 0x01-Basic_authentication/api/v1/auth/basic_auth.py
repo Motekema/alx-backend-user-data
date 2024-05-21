e67764e6ec932859507c5704f86aeafa459d1fa8
Motekema <motekema@gmail.com>
2024-05-21 10:26:25 +0000

    My commit

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,7 +8,7 @@ class BasicAuth(Auth):
     """ BasicAuth class that inherits from Auth """
 
     def extract_base64_authorization_header(self, authorization_header: str) -> str:
-        """ Extracts the Base64 part of the Authorization header for Basic Authentication """
+        """ Extracts the Base64 part of the Authorization """
         if authorization_header is None:
             return None
         if not isinstance(authorization_header, str):

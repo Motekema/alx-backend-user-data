5acfaf9b1d5069b99394da4a91df7d3e0602ca70
Motekema <motekema@gmail.com>
2024-05-30 09:54:20 +0000

    My commit

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+User model
+"""
+
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+    """
+    User model for the users table
+    """
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
+
+# This is just for demonstration; usually, you don't include this in the model file.
+if __name__ == "__main__":
+    from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine('sqlite:///:memory:')
+    Base.metadata.create_all(engine)
+    
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    print(User.__tablename__)
+    for column in User.__table__.columns:
+        print("{}: {}".format(column, column.type))
